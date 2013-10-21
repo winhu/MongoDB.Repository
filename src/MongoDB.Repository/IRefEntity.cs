745a08dd45f95365d574180f5b1f7447218e2e88
winhu <winhu80@gmail.com>
2013-10-21 09:21:12 +0800

    .

---
+++ src/MongoDB.Repository/IRefEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MongoDB.Repository
+{
+    public interface IRefEntity : IEntity
+    {
+        /// <summary>
+        /// list of MongoDBRef
+        /// </summary>
+        List<MongoDBRef> DBRefs { get; set; }
+
+        /// <summary>
+        /// save IEntity first, then save list of MongoDBRef
+        /// </summary>
+        void Update();
+    }
+}
